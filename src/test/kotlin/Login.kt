7544cdab82e1c267d642ceae1aab6a5f686bb2f3
rogeriocr23b <rogeriocr23@gmail.com>
2022-09-11 18:02:34 -0300

    adição de funcionalidades

--- src/test/kotlin/Login.kt
+++ src/test/kotlin/Login.kt
@@ -4,7 +4,9 @@ import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class Login : Base() {
 
 
